1b11cc13ce665a95a78fca7746d41fb321e49696
faqahat <faqahatk@gmail.com>
2026-05-08 07:14:22 +0500

    fix: cloudflare adapter

--- src/pages/api/info.js
+++ src/pages/api/info.js
@@ -2,7 +2,7 @@ export const prerender = false;
 
 export async function GET() {
   try {
-    const res = await fetch("http://185.107.96.148:3000/query");
+    const res = await fetch("https://query.ls-sw.net/query");
     const data = await res.json();
     return new Response(JSON.stringify(data), {
       status: 200,
